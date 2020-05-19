0c81cc784d69d229cadbd33caf625687c5de78d7
BeardKoda <akinsuyi.joshua84@gmail.com>
2020-05-19 01:03:12 +0100

    Added G

--- src/app/controllers/user/songController.js
+++ src/app/controllers/user/songController.js
@@ -178,8 +178,8 @@ let controller = {
             status: 1,
             is_deleted:0,
             premium:'0',
-            title:{ $not: null},
-            level:{$not:'0'}
+            title:{ [Op.ne]: null},
+            level:{[Op.ne]:'0'}
         }});
         let page = parseFloat(req.query.page )|| 1;      // page number
         let pages = Math.ceil(data.count / limit);
