d3b539412d5717f0c041cea19779e847723e1dcf
BeardKoda <akinsuyi.joshua84@gmail.com>
2020-05-29 11:09:06 +0100

    Almost

--- src/app/controllers/user/searchController.js
+++ src/app/controllers/user/searchController.js
@@ -10,49 +10,48 @@ let controller = {
         // console.log(query)
         if(query){
             const artists = await models.Artist.findAll({
-                attributes: ['id', 'name', 'cover_img'],
+                attributes: ['id', 'name', ['cover_img', 'image']],
                 limit: limit,
                 offset: offset,
                 where: {
                     status: 1,
                     is_deleted:0,
                     name: {
-                        [Op.like]: query+'%'
-                      }
+                        [Op.like]:  '%'+query+'%'
+                    }
                 },
-                $sort: { id: 1 }
+                $sort: { id: 1 },
+                include:[{model:models.Artist_Profile, as:'profile', required:true, attributes:['stage_name']}]
             });
             const songs = await models.Song.findAll({
-                attributes: ['id', 'title', 'cover_img'],
+                attributes: ['id', 'title', ['cover_img', 'image']],
                 limit: limit,
                 offset: offset,
                 where: {
                     status: 1,
                     is_deleted:0,
-                    title: {[Op.like]: query+'%'}
+                    title: {[Op.like]: '%'+query+'%'}
                 },
                 include: [
-                    {model:models.Artist, as:'artist', attributes:['id', 'name']},
+                    {model:models.Artist, as:'artist',  attributes:[['uuid','id'], 'name'], include:[{model:models.Artist_Profile, as:'profile', required:true, attributes:['stage_name']}]},
                     {model:models.Album, as:'album', attributes:['id', 'title']}
                 ],
                 $sort: { id: 1 }
             });
             const albums = await models.Album.findAll({
                 attributes: ['id', 'title', 'cover_img'],
-                limit: limit, offset: offset, where: { status: 1, is_deleted:0, title: { [Op.like]: query+'%'}},
+                limit: limit, offset: offset, where: { status: 1, is_deleted:0, title: { [Op.like]: '%'+query+'%'}},
                 $sort: { id: 1 }
             });
             // console.log(albums)
-            let data = [
-                {
+            let data ={
                     songs:songs,
                     albums:albums,
-                    artist:artists
-                }
-            ]
-            res.status(200).send(data)
+                    artists:artists
+            }
+            res.status(200).json({data})
         }else{
-            res.status(422).send([null])
+            res.status(422).json([null])
         }
     }
 }
